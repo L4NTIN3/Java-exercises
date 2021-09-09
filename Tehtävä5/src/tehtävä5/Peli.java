ff421fa2c81c2db017c695b27bed7d6993947fcb
Tommi Virtanen <tommiv01@outlook.com>
2021-09-09 20:16:16 +0300

    lisätty kommentit tehtävään 5

--- Tehtävä5/src/tehtävä5/Peli.java
+++ Tehtävä5/src/tehtävä5/Peli.java
@@ -4,35 +4,46 @@ import java.util.Scanner;
 
 public class Peli{
 
+    //alustetaan pelissä tarvittavat muuttujat.
     private int kone;
     private int pelaaja;
     private String errMsg;
     
+    //setter metodi koneen veikkaamalle arvolle
     public void setKone(int x){
         this.kone = x;
     }
-    
+    //setter metodi pelaajan veikkaamalle arvolle
     public void setPelaaja(int x){
         this.pelaaja = x;
     }
-    
+    //setter metodi errormessagelle, ei tarvetta, mutta lisää osaamista ja 
+    //laatimiseen tulee toistoa.
     public void setError(){
         this.errMsg = "Jotain meni pieleen.";
     }
-    
+    //getter samaiselle errorille.
     public String errorMsg() {
         return errMsg;
     }
     
+    //staattinen metodi, joka suorittaa ohjelman.
     static void pelaaPeli(){
         
+        //luodaan peli objekti jotta päästään käsiksi edellä mainittuihin arvoihin.
         Peli peli = new Peli();
         
+        //suoritetaan try-catch metodi. Tässä ei nyt ole mitään käytännön tilannetta 
+        //joka antaisi erroria, mutta virkistykseksi tämäkin täällä. 
         try{
             
             peli.setKone(randomNumero());
             peli.setPelaaja(kysyNumero());
             
+            /* peli kysyy käyttäjältä lukua niin kauan kunnes oikea veikkaus on annettu. 
+            vertailu tapahtuu if statementeilla ja vastauksen jälkeen annetaan vastausta 
+            kuvaava printtaus.
+            */
             while(peli.pelaaja != peli.kone){
             
                 System.out.print("Väärä vastaus. ");
@@ -62,7 +73,7 @@ public class Peli{
         
     }
     
-    
+    //erillinen metodi pelaajan antaman numeron kysyntään.
     static int kysyNumero(){
         System.out.print("Give a number: ");
         
@@ -72,6 +83,7 @@ public class Peli{
         return v;
     }
     
+    //erillinen metodi koneen arpomalle luvulle.
     static int randomNumero(){
         Random rand = new Random();
         
--- Tehtävä5/src/tehtävä5/Tehtävä5.java
+++ Tehtävä5/src/tehtävä5/Tehtävä5.java
@@ -2,8 +2,9 @@ package tehtävä5;
 
 
 public class Tehtävä5 {
-    
+    //aloitetaan ohjelman suoritus
     public static void main(String[] args) {
+        //kutsutaan peli luokasta metodia pelaaPeli
      
         Peli.pelaaPeli();
     }
